f48135de079c7356cb22ef5b09dad9c43190c1e8
Jerome Meuleman <jmeulema@fu-r6-p4.s19.be>
2022-09-12 16:28:32 +0200

    :sparkles: first

---
+++ Inter/inter.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmeulema <jmeulema@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 15:43:07 by jmeulema          #+#    #+#             */
+/*   Updated: 2022/09/12 16:26:21 by jmeulema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	check_repeat(char *str, char c, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (ac == 3)
+	{
+		while (av[1][i])
+		{
+			j = 0;
+			while (av[2][j])
+			{
+				if ((av[1][i] == av[2][j]) && check_repeat(av[1], av[1][i], i) == 1)
+				{
+					write (1, &av[1][i], 1);
+					break;
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
---
+++ union/union.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   union.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmeulema <jmeulema@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 15:46:10 by jmeulema          #+#    #+#             */
+/*   Updated: 2022/09/12 15:59:18 by jmeulema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	check_repeat(char *str, char c, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_strlcat(char *s1, char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+		i++;
+	while (s2[i])
+	{
+		s1[i] = s2[j];
+		i++;
+		j++;
+	}
+	s1[i] = '\0';
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 0;
+	if (ac == 3)
+	{
+		ft_strlcat(av[1], av[2]);
+		while (av[1][i])
+		{
+			if (check_repeat(av[1], av[1][i], i) == 1)
+				write (1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write (1, "\n", 1);
+}
